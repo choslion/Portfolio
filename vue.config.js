b6ec0ce0362b4a01d826283d7fc5c6b8ef7ffcb3
Eddy <cshyeok24@gmail.com>
2023-02-02 15:28:08 +0900

    firebase config.js path추가

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: "/portfolio/dist",
+  publicPath: '/portfolio/dist',
+  publicPath: '',
 });
